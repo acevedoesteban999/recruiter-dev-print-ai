f109ea7a50863eee93bd427b3d4feb838b24a0c9
Esteban <acevedoesteban999@gmail.com>
2025-04-16 21:41:54 -0400

    [ADD scrape_base: Add scrape base for selenium. [ADD] scrape_hn: Add Scrape for News

--- backend/app/scrape_books.py
+++ backend/app/scrape_books.py
@@ -1,18 +1,11 @@
 import os
 import json
-import time
 import logging
-from typing import Dict
+from src.scrape_base import ScrapeBase
 from bs4 import BeautifulSoup
-from selenium import webdriver
-
-from selenium.webdriver.firefox.service import Service 
-from selenium.webdriver.firefox.options import Options
-from webdriver_manager.firefox import GeckoDriverManager
 
 from selenium.webdriver.common.by import By
 
-from selenium.webdriver.support.ui import WebDriverWait
 import redis
 from dotenv import load_dotenv
 from urllib.parse import urljoin
@@ -26,14 +19,11 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 
-class BookScraper:
-    @staticmethod
-    def __str_2_int(_str:str|None)->int:
-        try:
-            return int(_str)
-        except:
-            return 0
+class BookScraper(ScrapeBase):
+    
+    
     def __init__(self):
+        super().__init__()
         self.redis_client = redis.Redis(
             host=os.getenv("REDIS_HOST",'localhost'),
             port=self.__str_2_int(os.getenv("REDIS_PORT")),
@@ -42,22 +32,15 @@ class BookScraper:
         if(not self.redis_client.ping()):
             logger.error(f"Redis Error: Server dont response correctly")
             
-        self.driver = self._init_selenium()
+        
+        
         self.scraped_books = 0
-        self.BASE_URL = str(os.getenv("BASE_URL",""))
+        self.BASE_BOOKS_URL = str(os.getenv("BASE_BOOKS_URL",""))
         self.MAX_PRICE = self.__str_2_int(os.getenv("MAX_PRICE","5"))
         self.MAX_BOOKS = self.__str_2_int(os.getenv("MAX_BOOKS","100"))
         self.MIN_BOOKS = self.__str_2_int(os.getenv("MIN_BOOKS","50"))
 
-    def _init_selenium(self):
-        _options = Options()
-        _options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
-        # _options.add_argument("--headless")
-        # _options.add_argument("--no-sandbox")
-        return webdriver.Firefox(
-            service=Service(GeckoDriverManager().install()),
-            options=_options,
-        )
+    
 
     def _scrape_book_details(self, book_url: str):
         self.driver.get(book_url)
@@ -66,7 +49,7 @@ class BookScraper:
             title = soup.find("h1").text
             price = float(soup.find("p", class_="price_color").text[1:]) 
             category = soup.find("ul", class_="breadcrumb").find_all("a")[2].text
-            image_url = self.BASE_URL + soup.find("img")["src"].replace("../", "")
+            image_url = self.BASE_BOOKS_URL + soup.find("img")["src"].replace("../", "")
         except:
             return False
         return {
@@ -79,7 +62,7 @@ class BookScraper:
     def scrape_books(self):
         logger.info("Init scraping")
         try:
-            self.driver.get(self.BASE_URL)
+            self.driver.get(self.BASE_BOOKS_URL)
             
             while self.scraped_books < self.MAX_BOOKS:
                 soup = BeautifulSoup(self.driver.page_source, "html.parser")
@@ -117,8 +100,15 @@ class BookScraper:
             logger.error(f"Scraping Error: {e}")
         
         logger.info(f"Scraping Commpleted. Books find: {self.scraped_books}")
-        self.driver.quit()
-            
+    
+    @staticmethod
+    def __str_2_int(_str:str|None)->int:
+        try:
+            return int(_str) if _str else 0
+        except:
+            return 0
+    
 if __name__ == "__main__":
     scraper = BookScraper()
     scraper.scrape_books()
+    scraper._close_selenium()
