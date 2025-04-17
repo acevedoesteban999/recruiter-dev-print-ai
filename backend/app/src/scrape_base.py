87773b0e31e39b0e065cf910034c87914b673687
Esteban <acevedoesteban999@gmail.com>
2025-04-16 21:50:49 -0400

    [UPD] PEP8

--- backend/app/src/scrape_base.py
+++ backend/app/src/scrape_base.py
@@ -1,26 +1,22 @@
+import os
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service 
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 
-from selenium import webdriver
-
 
 class ScrapeBase():
-    
-    
+
     def __init__(self) -> None:
         _options = Options()
-        _options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+        _options.binary_location = str(os.getenv("FIREFOX_DRIVER_PATH", ""))
         # _options.add_argument("--headless")
         # _options.add_argument("--no-sandbox")
         self.driver = webdriver.Firefox(
             service=Service(GeckoDriverManager().install()),
             options=_options,
-        )   
-       
-    
-        
+        )
+
     def _close_selenium(self):
         if self.driver:
             self.driver.quit()
